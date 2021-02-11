0d2d10d1a154dcf4b448425b8fdfd796bee3af53
Zach Albia <zach.albia@gmail.com>
2021-02-11 23:34:23 +0800

    add more comments to time complexity problems

--- src/main/scala/lessons/_03/time_complexity/_1/FrogJump.scala
+++ src/main/scala/lessons/_03/time_complexity/_1/FrogJump.scala
@@ -4,7 +4,7 @@ package lessons._03.time_complexity._1
 // X ≤ Y.
 object Solution {
   // more math problem than programming...
-  def solution(x: Int, y: Int, d: Int): Int = {
+  def solution(x: Int, y: Int, d: Int): Int = { // 100% | T: O(1) | S: O(1)
     val delta = y - x
     delta / d + (if (delta % d == 0) 0 else 1)
   }
--- src/main/scala/lessons/_03/time_complexity/_3/TapeEquilibrium.scala
+++ src/main/scala/lessons/_03/time_complexity/_3/TapeEquilibrium.scala
@@ -5,7 +5,7 @@ package lessons._03.time_complexity._3
 // array has [2..100,000] elements
 object Solution {
   // we build prefix & suffix sums, then find the minimum absolute difference.
-  // iterators minimize allocations
+  // iterators minimize allocating intermediate arrays
   def solution(a: Array[Int]): Int = { // 100% | time: O(n) | space: O(n)
     val prefixSum = a.scan(0)(_ + _).toIterator.slice(1, a.length)
     val suffixSum = a.scanRight(0)(_ + _).toIterator.slice(1, a.length)
