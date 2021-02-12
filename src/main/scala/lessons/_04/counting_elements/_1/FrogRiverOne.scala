8d5767683f0401e08695982c7e0570241282e783
Zach Albia <zach.albia@gmail.com>
2021-02-12 10:28:20 +0800

    refactor & add tests for all FrogRiverOne solutions

--- src/main/scala/lessons/_04/counting_elements/_1/FrogRiverOne.scala
+++ src/main/scala/lessons/_04/counting_elements/_1/FrogRiverOne.scala
@@ -39,33 +39,24 @@ import scala.util.Random
 
 object FrogRiverOneTests extends TestSuite {
   val random = new Random()
-  val f = Solution.solution _
+  val solutions = Array(Solution.solution _, Alternative.solution _)
 
   val tests = Tests {
-    test("example1") {
-      val result = f(5, Array(1, 3, 1, 4, 2, 3, 5, 4))
-      assert(result == 6)
-    }
-    test {
-      val result = f(2, Array(1))
-      assert(result == -1)
-    }
-    test {
-      val result = f(2, Array(1))
-      assert(result == -1)
-    }
-    test("extreme") {
-      val result = f(100000, (1 to 100000).toArray)
-      assert(result == 99999)
-    }
-    test("random extreme") {
-      val result = f(100000, random.shuffle[Int, IndexedSeq](1 to 100000).toArray)
-      assert(result == 99999)
-    }
-    test("1 short of max") {
-      assert(f(100000, (1 to 99999).toArray) == -1)
+    test("example1") { check(5, Array(1, 3, 1, 4, 2, 3, 5, 4), 6) }
+    test { check(2, Array(1), -1) }
+    test { check(2, Array(1), -1) }
+    test("extreme") { check(100000, (1 to 100000).toArray, 99999) }
+    test("random extreme") { check(100000, random.shuffle[Int, IndexedSeq](1 to 100000).toArray, 99999) }
+    test("1 short of max") { check(100000, (1 to 99999).toArray, -1) }
+  }
+
+  def check(n: Int, a: Array[Int], expected: Int): Unit = {
+    solutions.foreach { f =>
+      val result = f(n, a)
+      assert(result == expected)
     }
   }
+
 }
 
 
