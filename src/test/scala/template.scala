5944663c69ebe157edf824c8e10ba2157a93916a
Zach Albia <zach.albia@gmail.com>
2021-02-16 15:21:24 +0800

    add min & max to template

--- src/test/scala/template.scala
+++ src/test/scala/template.scala
@@ -4,10 +4,13 @@ import scala.util.Random
 
 object SolutionTests extends TestSuite {
   val random = new Random()
-  val f = Solution.solution _
+  val f      = Solution.solution _
 
   val tests = Tests {
 //    test(check(???, ???))
+    val min  = Int.MinValue
+    val max  = Int.MaxValue
+    val maxN = 100000
   }
 
   def check(a: Nothing, expected: Nothing): Unit = {
